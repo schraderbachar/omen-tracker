b8794510e81b0477e23c96db956ab89b29973d5e
John SchraderBachar <johns3@iastate.edu>
2024-05-16 16:13:49 -0500

    got all omens to render

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,50 @@
 import "./App.css"
 import React, { useState, useEffect } from "react"
-import twoCrows from "./img/twoCrows.jpg"
 import data from "./db.json"
 
 function App() {
-  const [omens, setOmens] = useState([data])
-
+  const [omens, setOmens] = useState(data)
   useEffect(() => {}, [])
 
   return (
     <div>
       <nav className="navbar primC">
         <div className="container-fluid">
-          <span className="navbar-brand mb-2 h1">Omen Tracker</span>
+          <span className="navbar-brand mb-2 h1 text-white">Omen Tracker</span>
         </div>
       </nav>
       <div className="body">
+        <div className="mt-5">
+          <button type="button" className="btn primC text-white">
+            Add new Omen
+          </button>
+        </div>
         <h2 className="text-center mt-5">Omens</h2>
-        {omens &&
-          omens.length > 0 &&
-          omens.map((o, i) => (
-            <div className="card mb-3" key={i}>
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <img src={twoCrows} className="card-img-top" alt="..." />
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h5 className="card-title">{o[i].animal}</h5>
-                    <p className="card-text">{o[i].meaning}</p>
-                    <p className="card-text">
-                      <small className="text-body-secondary">
-                        Last seen: {o[i].datesSeen[0].date}
-                        {console.log(o[i])}
-                      </small>
-                    </p>
-                  </div>
+        {omens.map((o, i) => (
+          <div className="card mb-3" key={i}>
+            {console.log(o)}
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src={o.img} className="card-img-top" alt="add image" />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">{o.animal}</h5>
+                  <p className="card-text">{o.meaning}</p>
+                  <p className="card-text">
+                    <small className="text-body-secondary">
+                      Last seen: {o.datesSeen[0].date}
+                    </small>
+                    <br></br>
+                    <small className="text-body-secondary">
+                      {o.datesSeen.addContext}
+                    </small>
+                  </p>
                 </div>
               </div>
             </div>
-          ))}
+          </div>
+        ))}
       </div>
     </div>
   )
