d1e1ff1ea9afcd295f0d3a537733c5f15b98c457
John SchraderBachar <johns3@iastate.edu>
2024-05-17 13:56:06 -0500

    got all omens added, can now add a new omem- just need to create backend to update json file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,25 @@ import data from "./db.json"
 
 function App() {
   const [omens, setOmens] = useState(data)
-  useEffect(() => {}, [])
+
+  useEffect(() => {}, [omens])
+
+  const handleSubmit = (e) => {
+    // Prevent the browser from reloading the page
+    e.preventDefault()
+
+    // Read the form data
+    const form = e.target
+    const formData = new FormData(form)
+    //add date
+    formData.append("date", new Date(Date.now()).toLocaleDateString())
+    formData.append("img", "")
+
+    // Or you can work with it as a plain object:
+    const formJson = Object.fromEntries(formData.entries())
+    setOmens(...omens, { formJson })
+    console.log(omens)
+  }
 
   return (
     <div>
@@ -15,14 +33,84 @@ function App() {
       </nav>
       <div className="body">
         <div className="mt-5">
-          <button type="button" className="btn primC text-white">
+          <button
+            type="button"
+            className="btn primC text-white"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
             Add new Omen
           </button>
+          {/* modal */}
+          <div
+            className="modal fade"
+            id="exampleModal"
+            tabIndex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h1 className="modal-title fs-5" id="exampleModalLabel">
+                    Add new omen
+                  </h1>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div className="modal-body">
+                  <form method="post" onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                      <label className="form-label">Animal Name</label>
+                      <input
+                        name="animal"
+                        className="form-control"
+                        defaultValue=""
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label className="form-label">Meaning</label>
+                      <textarea
+                        name="meaning"
+                        className="form-control"
+                        defaultValue=""
+                        rows={4}
+                        cols={40}
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label className="form-label">Context</label>
+                      <textarea
+                        name="addContext"
+                        className="form-control"
+                        defaultValue=""
+                        rows={4}
+                        cols={40}
+                      />
+                    </div>
+
+                    <hr />
+                    <button
+                      type="submit"
+                      className="btn primC text-white"
+                      data-bs-dismiss="modal"
+                    >
+                      Add omen
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* end of modal */}
         </div>
         <h2 className="text-center mt-5">Omens</h2>
         {omens.map((o, i) => (
           <div className="card mb-3" key={i}>
-            {console.log(o)}
             <div className="row g-0">
               <div className="col-md-4">
                 <img src={o.img} className="card-img-top" alt="add image" />
@@ -33,11 +121,11 @@ function App() {
                   <p className="card-text">{o.meaning}</p>
                   <p className="card-text">
                     <small className="text-body-secondary">
-                      Last seen: {o.datesSeen[0].date}
+                      Last seen: {o.date}
                     </small>
                     <br></br>
                     <small className="text-body-secondary">
-                      {o.datesSeen.addContext}
+                      {o.addContext}
                     </small>
                   </p>
                 </div>
